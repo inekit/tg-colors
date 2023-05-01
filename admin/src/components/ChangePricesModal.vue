eef142dbe7cd6b0cc070180f6d85ca57d99c79ba
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-02 00:04:17 +0300

    fr

--- admin/src/components/ChangePricesModal.vue
+++ admin/src/components/ChangePricesModal.vue
@@ -4,9 +4,7 @@
     <CForm novalidate :validated="formValid" ref="add-file-form" @change="wregert" @submit.prevent="editPrices()"
       class="add-user" style="display: 'none'">
       <CModalHeader>
-        <CModalTitle>{{
-          mode === 'new' ? 'Добавить товар' : 'Редактировать товар'
-        }}</CModalTitle>
+        <CModalTitle>Сменить цены</CModalTitle>
       </CModalHeader>
       <CModalBody>
         <div class="options-shedle">
@@ -18,14 +16,6 @@
                   Материал</td>
                 <td v-for="sizeName in distinct_sizes" :key="'sizeh-' + sizeName">
                   {{ sizeName }}
-                  <CButton color="secondary" @click="dropSize(sizeName)">X</CButton>
-                </td>
-                <td>
-                  <CInputGroup class="">
-                    <CFormInput placeholder="Новый размер" type="text" v-model="tempSize" />
-                    <CButton class="add-button" type="button" color="secondary" @click="addSize(tempSize)"
-                      variant="outline">Добавить</CButton>
-                  </CInputGroup>
                 </td>
               </tr>
             </thead>
@@ -33,22 +23,16 @@
               <tr v-for="materialName in distinct_materials" :key="'material-' + materialName">
                 <td>
                   {{ materialName }}
-                  <CButton color="secondary" @click="dropMaterial(materialName)">X</CButton>
                 </td>
                 <td v-for="sizeName in distinct_sizes" :key="'size-' + sizeName">
-                  <CFormInput type="number" v-model.number="options_object[materialName][sizeName]" />
-                </td>
-                <td>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <CInputGroup class="">
-                    <CFormInput placeholder="Новый материал" type="text" v-model="tempMaterial" />
-                    <CButton class="add-button" type="button" color="secondary" @click="addMaterial(tempMaterial)"
-                      variant="outline">Добавить
-                    </CButton>
-                  </CInputGroup>
+                  <CFormInput type="number" @change="editOne(sizeName, materialName, true)" />
+                  <div
+                    v-if="edited_array.findIndex(el => el.material === 'materialName' && el.price === 'price' && el.is_backside === true) !== -1">
+                    {{
+                      options_object[materialName][sizeName] }}</div>
+                  <div v-else>
+                    <CButton color="secondary" @click="dropOne(sizeName, materialName, true)">X</CButton>
+                  </div>
                 </td>
               </tr>
             </tbody>
@@ -113,6 +97,7 @@ export default {
       distinct_sizes: [],
       tempSize: 0,
       tempMaterial: 0,
+      edited_array: [],
     }
   },
   async updated() {
@@ -162,40 +147,13 @@ export default {
           return false
         })
     },
-    addMaterial(name) {
-      let size_template = Object.values(this.options_object)?.[0] ?? {}
-      size_template = Object.fromEntries(Object.entries(size_template)?.map(([key]) => [key, 0]))
-      this.options_object[name] = size_template
-
-      size_template = Object.values(this.options_object_backside)?.[0] ?? {}
-      size_template = Object.fromEntries(Object.entries(size_template)?.map(([key]) => [key, 0]))
-      this.options_object_backside[name] = size_template
-
-      this.distinct_materials.push(name)
-      this.tempMaterial = ""
+    editOne(size, material, is_backside) {
+      this.edited_array.push({ size, material, is_backside });
+      console.log(this.edited_array)
     },
-    addSize(name) {
-      const new_oo_entries = Object.entries(this.options_object)?.map(([key, value]) => [key, Object.assign(value ?? {}, { [name]: 0 })])
-      this.options_object = Object.fromEntries(new_oo_entries)
-      const new_oob_entries = Object.entries(this.options_object_backside)?.map(([key, value]) => [key, Object.assign(value ?? {}, { [name]: 0 })])
-      this.options_object_backside = Object.fromEntries(new_oob_entries)
-
-      this.tempSize = ""
-      this.distinct_sizes.push(name)
-
-    },
-    dropMaterial(name) {
-      delete this.options_object[name]
-      delete this.options_object_backside[name]
-      this.distinct_materials = this.distinct_materials?.filter(el => el !== name)
-
-    },
-    dropSize(name) {
-      for (let key in this.options_object) {
-        delete this.options_object[key][name]
-        delete this.options_object_backside[key][name]
-      }
-      this.distinct_sizes = this.distinct_sizes?.filter(el => el !== name)
+    dropOne(size, material, is_backside) {
+      this.edited_array = this.edited_array.filter((el) => !(el.size === size.el.material === material, el.is_backside === is_backside));
+      console.log(this.edited_array)
     },
     changePrices() {
       eventBus.$emit('changePrices')
@@ -210,8 +168,7 @@ export default {
 
       var formData = new FormData()
 
-      formData.append('optionsObject', JSON.stringify(this.options_object))
-      formData.append('optionsObjectBackside', JSON.stringify(this.options_object_backside))
+      formData.append('edited_array', JSON.stringify(this.edited_array))
 
       return formData
     },

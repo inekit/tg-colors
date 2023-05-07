3faab7229d2e125dae7010a917a77e980a4fbe43
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-07 22:38:19 +0300

    y

--- webapp/backend/src/server/app.js
+++ webapp/backend/src/server/app.js
@@ -98,7 +98,7 @@ var getFiles = function (dir, files_) {
   files_ = files_ || [];
   var files = fs.readdirSync(dir);
   for (var i in files) {
-    var name = dir + "/" + files[i];
+    var name = files[i];
     if (fs.statSync(name).isDirectory()) {
       getFiles(name, files_);
     } else {

5027aba45ccba4a039087bffab6c6886178f01f4
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-13 16:54:38 +0300

    ef

--- webapp/frontend/src/views/Categories.vue
+++ webapp/frontend/src/views/Categories.vue
@@ -213,17 +213,9 @@ export default {
             }
 
             img {
-                position: absolute;
-                margin: auto;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                max-width: 200%;
-                max-height: 130%;
-                min-width: 100%;
-                min-height: 100%;
-                width: auto;
-                height: auto;
+                object-fit: cover;
+                width: 100%;
+                height: 100%;
             }
         }
 

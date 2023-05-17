43383f24bb7eec3674ceaf5cde0de98595457fd2
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-17 20:37:07 +0300

    ef

--- webapp/backend/src/server/utils/robokassa.js
+++ webapp/backend/src/server/utils/robokassa.js
@@ -10,9 +10,9 @@ class Robokassa {
     const signature = crypto
       .createHash("md5")
       .update(
-        `${this.MerchantLogin}:${OutSum}:${InvId}:${JSON.stringify(Reciept)}:${
-          this.Password
-        }`
+        `${this.MerchantLogin}:${OutSum}:${InvId}:${encodeURI(
+          JSON.stringify(Reciept)
+        )}:${this.Password}`
       )
       .digest("hex");
 

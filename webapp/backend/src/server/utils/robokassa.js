9a7a8c884ecd79ed3a0ded99abef774d5cef1d71
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-19 11:14:10 +0300

    ef

--- webapp/backend/src/server/utils/robokassa.js
+++ webapp/backend/src/server/utils/robokassa.js
@@ -42,7 +42,7 @@ class Robokassa {
           },
         })
         .then((response) => {
-          console.log(response.data);
+          console.log(response);
           resolve(
             `https://auth.robokassa.ru/Merchant/Index/${response.data?.invoiceID}`
           );

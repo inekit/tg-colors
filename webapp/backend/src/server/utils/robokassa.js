de6e6d7b77cd90fc6c114466af55760ab2012c9b
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-19 11:15:01 +0300

    ef

--- webapp/backend/src/server/utils/robokassa.js
+++ webapp/backend/src/server/utils/robokassa.js
@@ -30,7 +30,7 @@ class Robokassa {
 
     return new Promise((resolve, reject) => {
       axios
-        .post(`https://auth.robokassa.ru/Merchant/Index.aspx`, null, {
+        .post(`https://auth.robokassa.ru/Merchant/Indexjson.aspx`, null, {
           params: {
             MerchantLogin: this.MerchantLogin,
             OutSum,

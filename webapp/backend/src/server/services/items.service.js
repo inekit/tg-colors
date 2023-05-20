ddcf1a16d4d288ad2ce6fb21d7a8f3cfbb13916a
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-20 20:01:45 +0300

    ef

--- webapp/backend/src/server/services/items.service.js
+++ webapp/backend/src/server/services/items.service.js
@@ -322,6 +322,8 @@ class UsersService {
               )
             )?.[0]?.[0]?.id;
 
+            console.log(newId);
+
             if (!newId) {
               newId = (
                 await queryRunner.query(
@@ -334,31 +336,8 @@ class UsersService {
             idArray.push(newId);
           }
 
-          for (let optionIndex in oa_parsed_backside) {
-            const { material, size, price } = oa_parsed_backside[optionIndex];
-
-            let newId = (
-              await queryRunner.query(
-                `update item_options set price=$4 
-              where size=$2 and material=$3 and is_backside = true and item_id = $1 returning id`,
-                [item.id, size, material, price]
-              )
-            )?.[0]?.[0]?.id;
-
-            if (!newId) {
-              newId = (
-                await queryRunner.query(
-                  `insert into item_options (item_id,size,material,price,is_backside) values ($1,$2,$3,$4, true) returning id`,
-                  [item.id, size, material, price]
-                )
-              )?.[0]?.id;
-            }
-
-            idArray.push(newId);
-          }
-
           await queryRunner.query(
-            "delete from item_options where item_id = $1 and not (id = any($2))",
+            "delete from item_options where item_id = $1 and is_backside=false and not (id = any($2))",
             [item.id, idArray]
           );
         }
@@ -388,7 +367,6 @@ class UsersService {
     optionsArrayBackside,
     type,
     order_id,
-    editAll,
   }) {
     return new Promise(async (res, rej) => {
       const images_array = Array.isArray(images) ? images : [images];
@@ -541,7 +519,7 @@ class UsersService {
             [id, idArray]
           );
 
-          await this.editAllOptionsInCategory({
+          this.editAllOptionsInCategory({
             categoryName,
             oa_parsed,
             oa_parsed_backside,
